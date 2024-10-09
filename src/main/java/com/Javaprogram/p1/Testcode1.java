615e6bb5f2cd9b10cc44c514da37343ca741c7e6
suman shetty <suman shetty@DESKTOP-4RGDKE7>
2024-10-09 16:37:08 +0530

    My First Commit

---
+++ src/main/java/com/Javaprogram/p1/Testcode1.java
@@ -0,0 +1,9 @@
+package com.Javaprogram.p1;
+
+public class Testcode1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/main/java/com/Javaprogram/p1/package-info.java
@@ -0,0 +1,1 @@
+package com.Javaprogram.p1;
